7d4a31d258f1b59b37b680615f3d039900ab0382
- <moonjw727@gmail.com>
2026-06-10 18:43:58 +0900

    Feat: 오타 찾기

--- weather/src/store/store.js
+++ weather/src/store/store.js
@@ -1,24 +1,25 @@
-import { create } from "zustand";
-import axios from "axios";
+import {create} from 'zustand';
+import axios from 'axios';
 
 const useStore = create((set, get) => ({
-  weather: null,
+  weatherData: null,
   city: "",
 
   setCity: (city) => set({ city }),
 
   fetchWeather: async () => {
-    const { city } = get();
+const { city } = get();
 
-    try {
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
-          import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
-      );
-      set({ weather: response.data });
-    } catch (error) {
-      console.error(error);
-    }
+try {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${
+      import.meta.env.VITE_WEATHER_API_KEY}&units=metric`
+    );
+    set({ weatherData: response.data });
+  }
+  catch (error) {
+    console.error( error);
+}
   }
 }))
 
